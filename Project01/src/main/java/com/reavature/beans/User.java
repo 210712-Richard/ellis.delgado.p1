22a68df79166b21b57b49e601346784c6900b60e
Ellis Delgado <ellisdelgado13@gmail.com>
2021-08-02 07:33:49 -0700

    Added a user file

--- Project01/src/main/java/com/reavature/beans/User.java
+++ Project01/src/main/java/com/reavature/beans/User.java
@@ -1,5 +1,16 @@
 package com.reavature.beans;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable{
+/*
+ * What does do our users need?
+ * 
+ *	1. Name
+ *	2. User Type
+ *	3. Email
+ *	4. Employee number
+ *		file - this  will be its own object me thinks
+ *	5. 
+ */
 }
