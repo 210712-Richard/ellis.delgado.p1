a5f01e8c6b4fa5af61e337054c30af684d4085c1
Ellis Delgado <ellisdelgado13@gmail.com>
2021-08-12 22:29:20 -0700

    finished form cont

--- Project01/src/main/java/com/revature/controller/FormContImp.java
+++ Project01/src/main/java/com/revature/controller/FormContImp.java
@@ -5,10 +5,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.revature.beans.Form;
+import com.revature.beans.Status;
 import com.revature.beans.User;
 import com.revature.factory.BeanFactory;
 import com.revature.service.FormService;
 import com.revature.service.FormServiceImp;
+import com.revature.util.S3Util;
 
 import io.javalin.http.Context;
 
@@ -38,18 +40,6 @@ public class FormContImp implements FormController{
 
 	@Override
 	public void getForm(Context ctx) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void getFile(Context ctx) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addFile(Context ctx) {
 	User loggedUser = ctx.sessionAttribute("loggedUser");
 		
 		if(loggedUser == null) {
@@ -57,32 +47,98 @@ public class FormContImp implements FormController{
 			ctx.status(401);
 			return;
 		}
-		String file = ctx.header("extension");
+		Form form = formSer.getEmployeeForm(loggedUser);
+		ctx.json(form);
+	}
+
+	@Override
+	public void getFile(Context ctx) {
+	User loggedUser = ctx.sessionAttribute("loggedUser");
 		
-		if(file == null) {
+		if(loggedUser == null) {
+			log.error("User not logged in");
+			ctx.status(401);
+			return;
+		}
+		Form form = formSer.getEmployeeForm(loggedUser);
+		String file = form.getFile();
+		
+		ctx.json(file);
+		
+	}
+
+	@Override
+	public void addFile(Context ctx) {
+	User loggedUser = ctx.sessionAttribute("loggedUser");
+	String fileName = ctx.pathParam("fileName");
+	
+	String userFile = formSer.getUserFile(loggedUser);
+		if(userFile == null) {
+					log.error("File Name null");
+					ctx.status(400);
+					return;
+			}
+		
+		if(loggedUser == null) {
+			log.error("User not logged in");
+			ctx.status(401);
+			return;
+		}
+		String filetype = ctx.header("extension");
+		
+		if(filetype == null) {
 			log.error("File null");
 			ctx.status(400);
 			return;
 		}
+		String key = fileName+ "."+ filetype;
+		S3Util.getInstance().uploadToBucket(key, ctx.bodyAsBytes());
 		
+		formSer.addFile(loggedUser, key);
+		ctx.json(userFile);
 	}
 
 	@Override
 	public void deleteForm(Context ctx) {
-		// TODO Auto-generated method stub
+	User loggedUser = ctx.sessionAttribute("loggedUser");
 		
+		if(loggedUser == null) {
+			log.error("User not logged in");
+			ctx.status(401);
+			return;
+		}
+		formSer.deleteForm(loggedUser);
 	}
 
 	@Override
 	public void udpateGrade(Context ctx) {
-		// TODO Auto-generated method stub
+		User loggedUser = ctx.sessionAttribute("loggedUser");
+		
+		String grade = ctx.pathParam("grade");
+		
+		if(loggedUser == null) {
+			log.error("User not logged in");
+			ctx.status(401);
+			return;
+		}
+		Form form = formSer.getEmployeeForm(loggedUser);
+		form.setGrade(grade);
 		
 	}
 
 	@Override
 	public void updateStatus(Context ctx) {
-		// TODO Auto-generated method stub
+User loggedUser = ctx.sessionAttribute("loggedUser");
 		
+		Status status = Status.valueOf(ctx.pathParam("status"));
+		
+		if(loggedUser == null) {
+			log.error("User not logged in");
+			ctx.status(401);
+			return;
+		}
+		Form form = formSer.getEmployeeForm(loggedUser);
+		form.setStatus(status);
 	}
 
 }
